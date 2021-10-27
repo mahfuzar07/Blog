d0a3313fba31f810e6c8450ffc7bf7119b588d1e
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-27 21:15:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Database connect successfull'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(5000, () => {
+  console.log('server is running');
+});
