f5902de61cc9fe2b2b2ea1228718ed98710ecc91
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-29 23:07:04 +0600

    blog-api

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config();
+const multer = require('multer');
+const path = require('path');
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -12,6 +13,35 @@ mongoose
     console.log(err);
   });
 
-app.listen(5000, () => {
+//Multer file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('file has been uploaded');
+});
+
+//JSON
+app.use(express.json());
+//routes
+const authRoute = require('./routes/auth');
+app.use('/auth', authRoute);
+const userRoute = require('./routes/users');
+app.use('/user', userRoute);
+const postRoute = require('./routes/posts');
+app.use('/post', postRoute);
+const categoriesRoute = require('./routes/categories');
+app.use('/cat', categoriesRoute);
+
+//file
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+app.listen(5050, () => {
   console.log('server is running');
 });
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: {
+      type: String,
+      default:
+        'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', UserSchema);
