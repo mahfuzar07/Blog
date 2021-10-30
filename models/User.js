db8cf59cc298e12966bb1cce3c5a825295656b00
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-30 23:29:59 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,8 +7,7 @@ const UserSchema = new mongoose.Schema(
     password: { type: String, required: true },
     profilePic: {
       type: String,
-      default:
-        'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg',
+      default: '',
     },
   },
   {
